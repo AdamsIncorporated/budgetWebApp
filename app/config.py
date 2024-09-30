675a53c68fd2f582d075f80b10dcd127c94add7a
Adams <Samuel.Adams@centralhealth.net>
2024-09-30 11:50:50 -0500

    initial commit

---
+++ app/config.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+class Config:
+    """Base configuration class."""
+    LOGGING_LEVEL = logging.DEBUG
+    LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    LOGGING_LOCATION = os.path.join(os.path.dirname(__file__), 'app.log')  # Log file location
+
+    @staticmethod
+    def init_app(app):
+        """Initialize logging."""
+        logging.basicConfig(
+            level=Config.LOGGING_LEVEL,
+            format=Config.LOGGING_FORMAT,
+            handlers=[
+                logging.FileHandler(Config.LOGGING_LOCATION),  # Log to file
+                logging.StreamHandler()  # Log to console
+            ]
+        )
