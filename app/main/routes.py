b2c1344e05facad5f194d1e9568ad8fc78d12414
Adams <Samuel.Adams@centralhealth.net>
2024-10-01 09:56:28 -0500

    added dummy data to visualize the table

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,31 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
 from repositories.db_manager import get_db_connection
+import random
 
 main = Blueprint("main", __name__, template_folder="templates", static_folder="static")
+
+data = [
+    "SALARIES WAGES CONTRACT LABOR",
+    "TOTAL LABOR COST",
+    "PHYSICIAN SERVICES",
+    "Purchased Services subtotal",
+    "Public Relations Subtotal",
+    "PURCHASED SERVICES TOTAL",
+    "SUPPLIES TOTAL",
+    "Facility Services Subtotal",
+    "Rent/Lease Subtotal",
+    "Professional Development Subtotal",
+    "OTHER GOODS AND SERVICES",
+    "TOTAL OPERATING EXPENSE",
+]
+
+data = {header: [random.randint(100, 999) for _ in range(20)] for header in data}
 
 
 @main.route("/")
 @main.route("/budget-entry")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 
 @main.route("/login", methods=["GET", "POST"])
