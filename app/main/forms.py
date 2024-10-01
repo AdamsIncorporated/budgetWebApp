03419ad058268d3c20ae0e67acbea564ae309e10
Adams <Samuel.Adams@centralhealth.net>
2024-10-01 11:53:04 -0500

    created budget form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    FormField,
+    DecimalField,
+    FieldList,
+)
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -33,3 +41,32 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+
+class Budget(FlaskForm):
+    account = StringField()
+    rad_description = StringField()
+    account_code = StringField()
+    one_year_prior_actual = DecimalField()
+    one_year_prior_budget = DecimalField()
+    one_year_prior_variance = DecimalField()
+    one_year_prior_percent = DecimalField()
+    two_year_prior_actual = DecimalField()
+    two_year_prior_budget = DecimalField()
+    two_year_prior_variance = DecimalField()
+    actual_to_date = DecimalField()
+    budget = DecimalField()
+    variance = DecimalField()
+    percent = DecimalField()
+    forecast = DecimalField()
+    proposed_budget = DecimalField()
+    yoy_change = DecimalField()
+    comments = StringField()
+    business_case_name = StringField()
+    business_case_amount = DecimalField()
+    total_budget = DecimalField()
+
+
+class Budgets(FlaskForm):
+    budget_items = FieldList(FormField(Budget), min_entries=1)
+    submit = SubmitField("Submit Budget")
