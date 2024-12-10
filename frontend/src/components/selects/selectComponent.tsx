534f4f0bbda1c2d61ed1ab9ca7a342b17fff2341
Sam Adams <samuel.grant.adams@gmail.com>
2024-12-10 15:35:18 -0600

    fixed standard select null option prop

--- frontend/src/components/selects/selectComponent.tsx
+++ frontend/src/components/selects/selectComponent.tsx
@@ -33,11 +33,8 @@ const SelectComponent: React.FC<SelectProps> = ({
       name={name}
       value={selectedValue}
       onChange={handleChange}
-      className="bg-white border shadow-md text-stone-700 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5"
+      className="bg-white shadow-md text-stone-700 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5"
     >
-      <option value="" disabled selected>
-        {placeholder || "--Select--"}
-      </option>
       {options.map((option) => (
         <option key={option.value} value={option.value} className="bg-white text-stone-500 ">
           {option.label}
