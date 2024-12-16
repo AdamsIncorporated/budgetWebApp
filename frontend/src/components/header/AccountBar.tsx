13af4a1ab220ca2a3cb840147e0f2199ce7a989d
Sam Adams <samuel.grant.adams@gmail.com>
2024-12-16 09:16:07 -0600

    added logout dispatch

--- frontend/src/components/header/AccountBar.tsx
+++ frontend/src/components/header/AccountBar.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../stores/store";
 
 function AccountBar() {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
   return (
-    <div className="rounded-md shadow-md bg-inherit text-white">
-      <div>ACCOUNT BAR</div>
-    </div>
+    isAuthenticated && (
+      <div className="rounded-md shadow-md bg-inherit text-white">
+        <div>ACCOUNT BAR</div>
+      </div>
+    )
   );
 }
 
--- frontend/src/components/header/Links.tsx
+++ frontend/src/components/header/Links.tsx
@@ -1,48 +1,114 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaHome, FaUser, FaSignInAlt, FaUserPlus, FaKey, FaCrown } from 'react-icons/fa';
-import { FaCalculator } from "react-icons/fa";
-
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaUser,
+  FaSignInAlt,
+  FaUserPlus,
+  FaKey,
+  FaCrown,
+  FaCalculator,
+} from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../stores/store";
+import interceptor from "../../app/axiosConfig";
+import { logOut } from "../../stores/slices";
 
 interface LinksProps {
-    toggleIsOpen: () => void;
+  toggleIsOpen: () => void;
 }
 
-const Links: React.FC<LinksProps> = ({toggleIsOpen}) => (
-  <ul className="flex flex-col space-y-4 text-stone-500"> 
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaHome className="mr-2" /> 
-      <Link to="/">Home</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaSignInAlt className="mr-2" /> 
-      <Link to="/auth/login">Login</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaUserPlus className="mr-2" /> 
-      <Link to="/auth/register">Register</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaUser className="mr-2" /> 
-      <Link to="/auth/account">Account</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaKey className="mr-2" /> 
-      <Link to="/auth/reset-password">Reset Password</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaCrown className="mr-2" /> 
-      <Link to="/auth/register-admin/:token">Register Admin</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaCalculator className="mr-2" /> 
-      <Link to="/budget/">Budget</Link>
-    </li>
-    <li className="flex items-center hover:text-stone-800 transition ease-in duration-50" onClick={toggleIsOpen}>
-      <FaCalculator className="mr-2" /> 
-      <Link to="/admin-dahsboard/">Admin Dashboard</Link>
-    </li>
-  </ul>
-);
+const Links: React.FC<LinksProps> = ({ toggleIsOpen }) => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
+  const logout = async () => {
+    try {
+      await interceptor.get("/auth/logout");
+      dispatch(logOut());
+    } catch (error: any) {
+      console.error("Logout failed:", error);
+    }
+  };
+
+  return (
+    <ul className="flex flex-col space-y-4 text-stone-500">
+      <li
+        className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+        onClick={toggleIsOpen}
+      >
+        <FaHome className="mr-2" />
+        <Link to="/">Home</Link>
+      </li>
+
+      {isAuthenticated ? (
+        <>
+          <li
+            className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+            onClick={toggleIsOpen}
+          >
+            <FaSignInAlt className="mr-2" />
+            <Link to="#" onClick={logout}>Logout</Link>
+          </li>
+          <li
+            className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+            onClick={toggleIsOpen}
+          >
+            <FaUser className="mr-2" />
+            <Link to="/auth/account">Account</Link>
+          </li>
+        </>
+      ) : (
+        <>
+          <li
+            className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+            onClick={toggleIsOpen}
+          >
+            <FaSignInAlt className="mr-2" />
+            <Link to="/auth/login">Login</Link>
+          </li>
+          <li
+            className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+            onClick={toggleIsOpen}
+          >
+            <FaUserPlus className="mr-2" />
+            <Link to="/auth/register">Register</Link>
+          </li>
+          <li
+            className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+            onClick={toggleIsOpen}
+          >
+            <FaKey className="mr-2" />
+            <Link to="/auth/reset-password">Reset Password</Link>
+          </li>
+          <li
+            className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+            onClick={toggleIsOpen}
+          >
+            <FaCrown className="mr-2" />
+            <Link to="/auth/register-admin/:token">Register Admin</Link>
+          </li>
+        </>
+      )}
+
+      <li
+        className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+        onClick={toggleIsOpen}
+      >
+        <FaCalculator className="mr-2" />
+        <Link to="/budget/">Budget</Link>
+      </li>
+      <li
+        className="flex items-center hover:text-stone-800 transition ease-in duration-50"
+        onClick={toggleIsOpen}
+      >
+        <FaCalculator className="mr-2" />
+        <Link to="/admin-dahsboard/">Admin Dashboard</Link>
+      </li>
+    </ul>
+  );
+};
 
 export default Links;
